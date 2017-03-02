ad2f7f157ca58926235dfb7dcc230715c3b1062c
aluizio <aluizio777@gmail.com>
2017-03-02 11:46:02 -0300

    Added the context response.

--- activityResource/views.py
+++ activityResource/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
+
 from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from activityResource.models import Activity
 from activityResource.serializers import ActivitySerializer
+
+from activityResource.context import context
 # Create your views here.
 
 class ActivityList(generics.ListCreateAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
 
+    def options(self, request, *args, **kwargs):
+        return Response(context, status=200)
+
 class ActivityDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Activity.objects.all()
     serializer_class = ActivitySerializer
+
+    def options(self, request, *args, **kwargs):
+        return Response(context, status=200)
+
+class ActivityContext(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return Response(context, status=200)
