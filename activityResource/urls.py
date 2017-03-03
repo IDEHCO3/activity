5487cdbcf72b40e9fe48a71fe63aea5ec73b4555
aluizio <aluizio777@gmail.com>
2017-03-03 19:38:42 -0300

    fixed a little bug

--- activityResource/urls.py
+++ activityResource/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import include, url
 from activityResource import views
 
 urlpatterns = [
-    url(r'^activities/', views.ActivityList.as_view(), name='list'),
-    url(r'^activities/(?P<pk>\d+)/', views.ActivityDetail.as_view(), name='detail'),
+    url(r'^activities/$', views.ActivityList.as_view(), name='list'),
+    url(r'^activities/(?P<pk>\d+)/?$', views.ActivityDetail.as_view(), name='detail'),
     url(r'^context/', views.ActivityContext.as_view(), name='context'),
 ]
